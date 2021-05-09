dad588d38da72e622db14711f66873eea4abaa48
tzyn (mac) <tzyn.wang@gmail.com>
2021-05-09 10:36:17 +0800

    Update coding style

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const bodyParser = require('body-parser')
+
 const generateTrashTalk = require('./public/javascripts/trash_talk_generator.js')
-
 const expressHandlebars = require('express-handlebars')
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+const handlebarsHelpers = require('handlebars-helpers')
+const bodyParser = require('body-parser')
+const helpers = handlebarsHelpers()
 
+app.engine('handlebars', expressHandlebars({ helpers: helpers, defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
@@ -17,18 +19,7 @@ app.get('/', (req, res) => {
 
 app.post('/generate', (req, res) => {
   const talk = generateTrashTalk(req.body.jobTitle)
-  let checked = {}
-  switch (req.body.jobTitle) {
-    case 'engineer':
-      checked = { engineer: true }
-      break
-    case 'designer':
-      checked = { designer: true }
-      break
-    case 'entrepreneur':
-      checked = { entrepreneur: true }
-  }
-  res.render('index', { talk, checked })
+  res.render('index', { talk, checked: req.body })
 })
 
 app.listen(port, () => {
